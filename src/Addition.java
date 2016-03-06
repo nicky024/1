36559f1d8aa65e037ed2868b46382af8fc9542a2
Nicholas Griscti Soler <nicholas.griscti.14@um.edu.mt>
2016-03-06 09:04:38 +0100

    going back

---
+++ src/Addition.java
@@ -0,0 +1,7 @@
+public class Addition{
+
+	public int add(int x, int y){
+
+		return x+y;
+    }
+}
---
+++ src/Subtract.java
@@ -0,0 +1,7 @@
+public class Subtract{
+
+	public int sub(int x, int y){
+
+		return x-y;
+    }
+}
